575ed8cf8471c94892749af6ae902af3ad4f7793
Tisanyuk Ruslan <ruslantisanykot@gmail.com>
2021-02-06 01:26:50 +0200

    connected base style and create folder and path for Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,26 +9,26 @@ export default function Profile({
   stats: { followers, views, likes },
 }) {
   return (
-    <div class="profile">
-      <div class="description">
-        <img class="avatar" src={avatar} alt="Аватар пользователя" />
-        <p class="name">{name}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
+    <div className="profile">
+      <div className="description">
+        <img className="avatar" src={avatar} alt="Аватар пользователя" />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className="stats">
         <li>
-          <span class="label">Followers </span>
-          <span class="quantity">{followers}</span>
+          <span className="label">Followers </span>
+          <span className="quantity">{followers}</span>
         </li>
         <li>
-          <span class="label">Views </span>
-          <span class="quantity">{views}</span>
+          <span className="label">Views </span>
+          <span className="quantity">{views}</span>
         </li>
         <li>
-          <span class="label">Likes </span>
-          <span class="quantity">{likes}</span>
+          <span className="label">Likes </span>
+          <span className="quantity">{likes}</span>
         </li>
       </ul>
     </div>

25cd2bbf6b85dd91ce82087b053ff02d0d0ddf8a
Tisanyuk Ruslan <ruslantisanykot@gmail.com>
2021-02-06 23:19:35 +0200

    added all style for Profile and task completed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import defaultImage from './default.jpg';
+import s from './Profile.module.css';
 
 export default function Profile({
   name,
@@ -9,26 +10,26 @@ export default function Profile({
   stats: { followers, views, likes },
 }) {
   return (
-    <div className="profile">
-      <div className="description">
-        <img className="avatar" src={avatar} alt="Аватар пользователя" />
-        <p className="name">{name}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img className={s.avatar} src={avatar} alt="Аватар пользователя" />
+        <p className={s.name}>{name}</p>
+        <p className={s.tag}>@{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
 
-      <ul className="stats">
+      <ul className={s.stats}>
         <li>
-          <span className="label">Followers </span>
-          <span className="quantity">{followers}</span>
+          <span className={s.label}>Followers </span>
+          <span className={s.quantity}>{followers}</span>
         </li>
         <li>
-          <span className="label">Views </span>
-          <span className="quantity">{views}</span>
+          <span className={s.label}>Views </span>
+          <span className={s.quantity}>{views}</span>
         </li>
         <li>
-          <span className="label">Likes </span>
-          <span className="quantity">{likes}</span>
+          <span className={s.label}>Likes </span>
+          <span className={s.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import style from './Profile/Profile.module.css';
 
 ReactDOM.render(
   <React.StrictMode>

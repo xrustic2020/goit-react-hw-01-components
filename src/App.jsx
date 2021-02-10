00e18497b6f7b481675e4a16f25cc6efe1f13fa4
Tisanyuk Ruslan <ruslantisanykot@gmail.com>
2021-02-10 18:03:54 +0200

    added base markup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import Section from './components/Section';
 import Profile from './components/Profile';
 import Statistics from './components/Statistics';
 import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
+
 import user from './user.json';
 import statisticsData from './statistical-data.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 
 export default function App() {
   return (
     <Section>
+      <TransactionHistory items={transactions} />
       <Profile
         name={user.name}
         tag={user.tag}
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import s from './TransactionHistory.module.css';
+
+export default function TransactionHistory({ items }) {
+  console.log(items[0]);
+  return (
+    <table className={s.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
